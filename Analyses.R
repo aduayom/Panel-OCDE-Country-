16c7681d5ebde5cb9df1f2e852c7e6c6a39085b8
Messan Daniel ADUAYOM MESSAN <64219283+aduayom@users.noreply.github.com>
2024-11-13 14:02:49 +0100

    Update Analyses.R

--- Analyses.R
+++ Analyses.R
@@ -7,8 +7,9 @@ library(corrplot)
 # Convertir Country en facteur (catégoriel) et Year en numérique
 Base$Country <- as.factor(Base$`Country name`)
 Base$Year <- as.numeric(as.character(Base$Year))
+str(Base)
 
-# Tracer le graphique
+# Tracer le graphique ESP
 ggplot(Base, aes(x = Year, y = ESP, color = Country)) +
   geom_line(linewidth = 1) +                # Remplacer size par linewidth pour ggplot2 >= 3.4.0
   geom_point(size = 2) +                    # Ajoute des points pour chaque observation
@@ -18,3 +19,30 @@ ggplot(Base, aes(x = Year, y = ESP, color = Country)) +
   theme_minimal() +                         # Applique un thème minimaliste
   theme(legend.title = element_blank()) +   # Supprime le titre de la légende
   scale_color_viridis_d()                   # Utilise une palette de couleurs distinctes (viridis)
+
+# PIBt
+ggplot(Base, aes(x = Year, y = PIBt, color = Country)) +
+  geom_line(linewidth = 1) +                # Remplacer size par linewidth pour ggplot2 >= 3.4.0
+  geom_point(size = 2) +                    # Ajoute des points pour chaque observation
+  labs(title = "Évolution de l'PIBt par Pays",
+       x = "Année",
+       y = "PIBt") +
+  theme_minimal() +                         # Applique un thème minimaliste
+  theme(legend.title = element_blank()) +   # Supprime le titre de la légende
+  scale_color_viridis_d()                   # Utilise une palette de couleurs distinctes (viridis)
+
+# Educ
+ggplot(Base, aes(x = Year, y = Educ, color = Country)) +
+  geom_line(linewidth = 1) +                # Remplacer size par linewidth pour ggplot2 >= 3.4.0
+  geom_point(size = 2) +                    # Ajoute des points pour chaque observation
+  labs(title = "Évolution de l'Educ par Pays",
+       x = "Année",
+       y = "Educ") +
+  theme_minimal() +                         # Applique un thème minimaliste
+  theme(legend.title = element_blank()) +   # Supprime le titre de la légende
+  scale_color_viridis_d()                   # Utilise une palette de couleurs distinctes (viridis)
+
+
+
+
+
